7f6e7478239eb3bb2401a11548a6bdd5a951360f
Lukas Röll <lukas-roell@gmail.com>
2016-05-27 16:49:50 +0200

    added a Sprite character with simple animation

--- RPGClashers/RPGClashers/modules/rpgclashers/states/game.js
+++ RPGClashers/RPGClashers/modules/rpgclashers/states/game.js
@@ -1,9 +1,15 @@
 ﻿/// <reference path="../vendor/phaser.js" />
 
-define([
-    "Phaser"
-], function (Phaser) {
 
+define([
+    "Phaser",
+
+
+], function (Phaser) {
+      
+   
+    var sprite;
+    var cursors;
     var GameState = function (game) {
 
     };
@@ -11,11 +17,31 @@ define([
     GameState.prototype = {
 
         constructor: GameState,
-
+        
         create: function () {
-            this.game.state.start("GameOver");
-           
+            this.game.stage.backgroundColor = '#787878';
+            
+            this.map = this.game.add.tilemap('testPoke');
+
+            this.map.addTilesetImage('PathAndObjects', 'tiles');
+            this.layer = this.map.createLayer('ground1');
+            this.layer.resizeWorld();
+            this.cursors = this.game.input.keyboard.createCursorKeys();
+            this.sprite = this.game.add.sprite(300, 200, 'skeleton');
+
+            this.sprite.animations.add('walk', [143,144,145,146,147,148,149,150,151]);
+
+            this.sprite.animations.play('walk', 10, true);
+
+            //this.game.state.start("GameOver");
         },
+
+        //update: function () {
+        //    if (this.cursors.up.isDown) {
+        //        sprite.position.y -= 8;
+
+        //    }
+        //}
     };
 
     return GameState;
--- RPGClashers/RPGClashers/modules/rpgclashers/states/preload.js
+++ RPGClashers/RPGClashers/modules/rpgclashers/states/preload.js
@@ -20,32 +20,30 @@
             this.load.setPreloadSprite(loadingBar);
 
             this.game.load.tilemap('testPoke', 'assets/tilemaps/maps/map.json', null, Phaser.Tilemap.TILED_JSON);
-
-            //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
-
             this.game.load.image('tiles', 'assets/tilemaps/maps/tileSet.png');
+            this.game.load.spritesheet('skeleton', 'assets/characters/skeleton.png', 64, 64);
 
         },
 
         create: function () {
-            //this.game.state.start("Game");
-            this.game.stage.backgroundColor = '#787878';
-
-
-
-            //  The 'mario' key here is the Loader key given in game.load.tilemap
-           this.map = this.game.add.tilemap('testPoke');
+          
+           // this.game.stage.backgroundColor = '#787878';
+           //   The 'mario' key here is the Loader key given in game.load.tilemap
+           //this.map = this.game.add.tilemap('testPoke');
            
-            //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
-            //  The second parameter maps this name to the Phaser.Cache key 'tiles'
-           this.map.addTilesetImage('World1Test', 'tiles');
+           //   The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
+           //   The second parameter maps this name to the Phaser.Cache key 'tiles'
+           //this.map.addTilesetImage('PathAndObjects', 'tiles');
 
-            //  Creates a layer from the World1 layer in the map data.
-            //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
-           this.layer = this.map.createLayer('ground1');
+           //   Creates a layer from the World1 layer in the map data.
+           //   A Layer is effectively like a Phaser.Sprite, so is added to the display list.
+           //this.layer = this.map.createLayer('ground1');
 
-            //  This resizes the game world to match the layer dimensions
-           layer.resizeWorld();
+           //   This resizes the game world to match the layer dimensions
+           //this.layer.resizeWorld();
+
+
+           this.game.state.start("Game");
         }
     };
 
