141e21002475478fe751df21407aaedcba4e3c6c
Lukas Röll <lukas-roell@gmail.com>
2016-06-15 13:53:03 +0200

    new version

--- RPGClashers/RPGClashers/modules/rpgclashers/phasergame.js
+++ RPGClashers/RPGClashers/modules/rpgclashers/phasergame.js
@@ -3,7 +3,7 @@
 ], function (Phaser) {
 
     var PhaserGame = function (w, h) {
-        return new Phaser.Game(w, h, Phaser.AUTO);
+        return new Phaser.Game(w, h, Phaser.AUTO); 
     };
 
     return PhaserGame;
